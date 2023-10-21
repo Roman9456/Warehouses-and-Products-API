d77beb69d8fd0a1081b7e4c82b47a1704604593f
Roman <smolaykov_ra@mail.ru>
2023-10-21 15:49:08 +0300

    Initial commit

---
+++ logistic/serializers.py
@@ -0,0 +1,57 @@
+
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+    @staticmethod
+    def get_products_by_title(query):
+        return Product.objects.filter(title__icontains=query)
+
+    @staticmethod
+    def get_products_by_description(query):
+        return Product.objects.filter(description__icontains=query)
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        positions_data = validated_data.pop('positions')
+        stock = Stock.objects.create(**validated_data)
+
+        for position_data in positions_data:
+            product = position_data.pop('product')
+            StockProduct.objects.create(stock=stock, product=product, **position_data)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions_data = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+
+        instance.positions.all().delete()
+
+        for position_data in positions_data:
+            product = position_data.pop('product')
+            StockProduct.objects.create(stock=stock, product=product, **position_data)
+
+        return stock
+
+    @staticmethod
+    def get_stocks_with_product(product_id):
+        return Stock.objects.filter(positions__product_id=product_id)
---
+++ logistic/views.py
@@ -0,0 +1,22 @@
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['title', 'description']
+    filterset_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['address']
+    filterset_fields = ['address']
